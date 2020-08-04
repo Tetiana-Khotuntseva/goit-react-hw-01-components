22e8ed444adb664650a8e22024d4cc34b4b4600c
t.khotuntseva <tanya.khotunzeva@gmail.com>
2020-08-04 12:44:04 +0300

    Updates

---
+++ precache-manifest.2bee8ba79a2c0f55224a743977494326.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9096f9796c876a59418545a68bcc8f1",
+    "url": "/github.io/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c948e23827d14d77f44f",
+    "url": "/github.io/goit-react-hw-01-components/static/css/main.7b1488a5.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c948e23827d14d77f44f",
+    "url": "/github.io/goit-react-hw-01-components/static/js/main.8b5bf98c.chunk.js"
+  },
+  {
+    "revision": "d543f3f7f76a2ad070f8",
+    "url": "/github.io/goit-react-hw-01-components/static/js/runtime-main.57b42ba3.js"
+  }
+]);
